2acc1a875242a91f3e14b951b440bf357be27ea3
UniqueDevStorm <storm@uniquecode.team>
2021-03-06 11:30:11 +0900

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Clock from 'react-live-clock';
+import 'semantic-ui-css/semantic.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <style>
+                    {`
+                        @import url('https://fonts.googleapis.com/css2?family=Gugi&display=swap');
+                        * {
+                            font-family: 'Gugi', cursive;
+                            text-align: center;
+                        }
+                    `}
+                </style>
+                <h1 style={{ fontSize: '70px' }}>WebClock</h1>
+                <hr style={{ width: '50vh', height: '5px', backgroundColor: 'black', borderRadius: '50px' }} />
+                <h1 style={{ fontSize: '60px', marginTop: '20vh' }}>
+                    <Clock format={'YYYY.MM.DD'} ticking={true} />
+                </h1>
+                <h1 style={{ fontSize: '100px' }}>
+                    <Clock format={'HH : mm : ss'} ticking={true} />
+                </h1>
+            </div>
+        )
+    }
 }
-
-export default App;
